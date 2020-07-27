aaa4f30144a5ffdea9b76d74fb4f6253059cd6b1
marta-kapolka <mkapolka@gmail.com>
2020-07-27 12:48:17 +0200

    feat: add hamburger button animation

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,10 +1,19 @@
 const hamburgerButton = document.querySelector('.hamburger--js');
 const navigationMenu = document.querySelector('.navigation--js');
+const sticks = document.querySelectorAll('.hamburger__stick');
 
 hamburgerButton.addEventListener('click', () => {
     navigationMenu.classList.toggle('navigation--open');
+    hamburgerButton.classList.toggle('hamburger--open');
+    for (const stick of sticks) {
+        stick.classList.toggle('hamburger--open')
+    }
 });
 
 navigationMenu.addEventListener('click', () => {
     navigationMenu.classList.remove('navigation--open');
+    hamburgerButton.classList.remove('hamburger--open');
+    for (const stick of sticks) {
+        stick.classList.remove('hamburger--open')
+    }
 });
