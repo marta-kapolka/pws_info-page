7bbb36e9ba642f0f0e1198f8f6d4e75703e463f7
marta-kapolka <mkapolka@gmail.com>
2020-07-27 13:21:58 +0200

    feat: prevent page scroll when mobile menu is open

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,12 +1,18 @@
 const hamburgerButton = document.querySelector('.hamburger--js');
 const navigationMenu = document.querySelector('.navigation--js');
 const sticks = document.querySelectorAll('.hamburger__stick');
+const body = document.querySelector('#body');
 
 hamburgerButton.addEventListener('click', () => {
     navigationMenu.classList.toggle('navigation--open');
     hamburgerButton.classList.toggle('hamburger--open');
     for (const stick of sticks) {
         stick.classList.toggle('hamburger--open')
+    }
+    if (navigationMenu.classList.contains('navigation--open')) {
+        body.classList.add('no-scroll')
+    } else {
+        body.classList.remove('no-scroll')
     }
 });
 
