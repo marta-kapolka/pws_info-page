9ae2af4d1af91a7dd174aede3d23bcdab8931d60
marta-kapolka <mkapolka@gmail.com>
2020-07-27 15:34:12 +0200

    fix: fix hamburger button behavior on click when scrolled

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,42 +1,66 @@
-const hamburgerButton = document.querySelector('.hamburger--js');
-const navigationMenu = document.querySelector('.navigation--js');
-const sticks = document.querySelectorAll('.hamburger__stick');
-const body = document.querySelector('#body');
+const hamburgerButton = document.querySelector(".hamburger--js");
+const navigationMenu = document.querySelector(".navigation--js");
+const sticks = document.querySelectorAll(".hamburger__stick");
+const body = document.querySelector("#body");
 
-hamburgerButton.addEventListener('click', () => {
-    navigationMenu.classList.toggle('navigation--open');
-    hamburgerButton.classList.toggle('hamburger--open');
-    for (const stick of sticks) {
-        stick.classList.toggle('hamburger--open')
-    }
-    if (navigationMenu.classList.contains('navigation--open')) {
-        body.classList.add('no-scroll')
-    } else {
-        body.classList.remove('no-scroll')
-    }
-});
+// FUNCTIONS
 
-navigationMenu.addEventListener('click', () => {
-    navigationMenu.classList.remove('navigation--open');
-    hamburgerButton.classList.remove('hamburger--open');
-    for (const stick of sticks) {
-        stick.classList.remove('hamburger--open')
-    }
-    body.classList.remove('no-scroll');
-});
-
-function changeHamburgerStyle() {
-    if (window.pageYOffset >= 1 && !navigationMenu.classList.contains('navigation--open')) {
-        hamburgerButton.classList.add('hamburger--scroll');
-        for (const stick of sticks) {
-            stick.classList.add('stick--scroll')
-        }
-    } else {
-        hamburgerButton.classList.remove('hamburger--scroll');
-        for (const stick of sticks) {
-            stick.classList.remove('stick--scroll')
-        }
-    }
+function blackHamburger() {
+  hamburgerButton.classList.remove("hamburger--scroll");
+  for (const stick of sticks) {
+    stick.classList.remove("stick--scroll");
+  }
 }
 
-window.addEventListener('scroll', changeHamburgerStyle);
+function whiteHamburger() {
+  hamburgerButton.classList.add("hamburger--scroll");
+  for (const stick of sticks) {
+    stick.classList.add("stick--scroll");
+  }
+}
+
+function changeHamburgerStyle() {
+  if (window.pageYOffset >= 1) {
+    whiteHamburger();
+  } else {
+    blackHamburger();
+  }
+}
+
+// CHANGE HAMBURGER BUTTON STYLE
+
+window.addEventListener("scroll", changeHamburgerStyle);
+
+// OPEN/CLOSE HAMBURGER MENU ON HAMBURGERBUTTON CLICK,
+// DISABLE/ENABLE PAGE SCROLL
+
+hamburgerButton.addEventListener("click", () => {
+  navigationMenu.classList.toggle("navigation--open");
+  hamburgerButton.classList.toggle("hamburger--open");
+  for (const stick of sticks) {
+    stick.classList.toggle("hamburger--open");
+  }
+  if (navigationMenu.classList.contains("navigation--open")) {
+    body.classList.add("no-scroll");
+    blackHamburger();
+  } else {
+    body.classList.remove("no-scroll");
+    if (window.pageYOffset >= 1) {
+      whiteHamburger();
+    } else {
+      blackHamburger();
+    }
+  }
+});
+
+// CLOSE HAMBURGER MENU ON CLICK ANYWHERE,
+// ENABLE PAGE SCROLL
+
+navigationMenu.addEventListener("click", () => {
+  navigationMenu.classList.remove("navigation--open");
+  hamburgerButton.classList.remove("hamburger--open");
+  for (const stick of sticks) {
+    stick.classList.remove("hamburger--open");
+  }
+  body.classList.remove("no-scroll");
+});
