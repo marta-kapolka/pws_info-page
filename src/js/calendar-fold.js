d251730494cee482067eb11d81980dead0807f0a
marta-kapolka <mkapolka@gmail.com>
2020-07-29 15:10:06 +0200

    feat: add resize event listener to calendar folding function

--- src/js/calendar-fold.js
+++ src/js/calendar-fold.js
@@ -1,32 +1,46 @@
 const events = document.querySelectorAll(".calendar__event--js");
-const containers = document.querySelectorAll('.calendar__button-container');
-const windowWidth = window.innerWidth;
+const containers = document.querySelectorAll(".calendar__button-container");
 
 function calendarFold() {
+  let windowWidth = window.innerWidth;
+  console.log(windowWidth);
   if (windowWidth < 768) {
-for (const container of containers) {
-    container.innerHTML = '<button class="calendar__button"></button>';
-}
+    for (const container of containers) {
+      container.innerHTML = '<button class="calendar__button"></button>';
+    }
     for (const event of events) {
       if (event.classList.contains("calendar__event--past")) {
         event.classList.add("calendar__event--fold");
       }
     }
+  } else {
+    for (const container of containers) {
+      container.innerHTML = "";
+    }
+    for (const event of events) {
+      event.classList.remove("calendar__event--fold");
+    }
   }
 }
 
 function listenFoldButton(button, i) {
-  button.addEventListener('click', function () { unFold(i); });
+  button.addEventListener("click", function () {
+    unFold(i);
+  });
 }
 
 function unFold(i) {
-  events[i].classList.toggle('calendar__event--fold');
+  events[i].classList.toggle("calendar__event--fold");
 }
 
 // INITIAL CALENDAR FORM SETUP BASED ON INITIAL VIEWPORT WIDTH
 calendarFold();
 
 // CREATE TABLE OF BUTTONS IF CREATED BY calendarFold()
-const buttons = document.querySelectorAll('.calendar__button')
+const buttons = document.querySelectorAll(".calendar__button");
 
 buttons.forEach(listenFoldButton);
+
+window.addEventListener("resize", function () {
+  calendarFold();
+});
