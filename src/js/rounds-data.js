e7afbcc01baf959778d8974336e45bfe7c61971f
marta-kapolka <mkapolka@gmail.com>
2023-09-04 20:14:13 +0200

    content update fix

--- src/js/rounds-data.js
+++ src/js/rounds-data.js
@@ -130,7 +130,7 @@ const ROUND_6 = {
   ],
   eventFacebookLink: undefined,
   eventWebsiteLink: undefined,
-  eventBulletinLink: "/public/rundy/2023_diablino_regulamin.pdf",
+  eventBulletinLink: "/rundy/2023_diablino_regulamin.pdf",
 };
 
 const ROUND_7 = {
